7d9dec3cb35d9e9ebfd4d634da8844d16584ece9
Aude Faucheux <aude53@gmail.com>
2019-07-28 12:28:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,15 +39,7 @@ def hit?#(adverse_card_total)
   # code hit? here
   prompt_user
   get_user_input
-  #  if get_user_input == "s"
-#    elsif get_user_input == "h"
-#      deal_card
-#      adverse_card_total += deal_card
-#    else
-#      puts "Please enter a valid command"
-#      prompt_user
-#    end
-    return get_user_input
+  return get_user_input
 end
 
 def invalid_command
