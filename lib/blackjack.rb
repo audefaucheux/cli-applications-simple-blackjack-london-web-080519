d617a365d559d1e95cd81dd135db0850f7ed179b
Aude Faucheux <aude53@gmail.com>
2019-07-28 12:18:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,8 +35,18 @@ def initial_round
   return sum
 end
 
-def hit?
+def hit?(adverse_card_total)
   # code hit? here
+  prompt_user
+  get_user_input
+    if get_user_input == "s"
+    elsif get_user_input == "h"
+      deal_card
+      player total = adverse_card_total + deal_card
+    else
+      puts "Please enter a valid command"
+      prompt_user
+    end    
 end
 
 def invalid_command
